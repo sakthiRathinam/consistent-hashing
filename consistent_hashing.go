4090cde700ab80ffdf60107dd07f7d80c5321e9e
sakthi <sakthiratnam050@gmail.com>
2025-05-31 19:47:31 +0530

    feat: added skeleton funcs for consistent hashing

---
+++ consistent_hashing.go
@@ -0,0 +1,6 @@
+package main
+
+/**First thing is i need to have a ring to place keys and nodes
+virtual node support to redistribute the keys properly accross nodes
+hashing alorgithm sha256 support
+Remove and add nodes function which redistrbute total_keys/number_of_nodes to minimize the damage**/
